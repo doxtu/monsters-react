4993e3bc2f398b7d04cc71b5b84b04ad4d192259
Nick Gonzales <nickwgonzales@gmail.com>
2020-07-09 10:25:41 -0500

    Updates

---
+++ precache-manifest.63f34144d214a73834d41858b6bee6eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4e249462acb4e62c0cb2bbac5d055a",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "8561b455ab3d022999b5",
+    "url": "/monsters-react/static/css/main.5dfea4d2.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-react/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8561b455ab3d022999b5",
+    "url": "/monsters-react/static/js/main.19ff197f.chunk.js"
+  },
+  {
+    "revision": "135c228377aeb18d7c64",
+    "url": "/monsters-react/static/js/runtime-main.c275f0be.js"
+  }
+]);
